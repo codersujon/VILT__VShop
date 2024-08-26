85ba505830c71a289aa1c68653e7550e8161df3a
unknown <csesujon155@gmail.com>
2024-08-25 22:42:39 -0700

    Flowbite admin dashboard design and install

---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+    import { onMounted } from 'vue'
+    import { initFlowbite } from 'flowbite'
+    import Navbar from './Navbar.vue'
+    import Sidebar from "./Sidebar.vue"
+
+    onMounted(() => {
+        initFlowbite();
+    })
+
+</script>
+
+<template>
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+        
+        <!-- navbar start -->
+        <Navbar />
+        <!-- end -->
+
+        <!-- Sidebar start -->
+        <Sidebar />
+        <!-- end -->
+
+       <slot />
+
+    </div>
+</template>
