47ba5f98457f04fe80c2584764374e43e61dee44
unknown <csesujon155@gmail.com>
2024-08-26 23:00:07 -0700

    Database Relation Making and product store logic create

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 use App\Models\Product;
+use App\Models\ProductImage;
 
 class ProductController extends Controller
 {
@@ -15,5 +17,40 @@ class ProductController extends Controller
     public function index(){
         $products = Product::get();
         return Inertia::render('Admin/Product/Index', ['products'=> $products]);
+    }
+
+    /**
+     * Store
+     */
+    public function store(Request $request){
+
+        $product = new Product();
+        $product->title = $request->title;
+        $product->quantity = $request->quantity;
+        $product->price = $request->price;
+        $product->description = $request->description;
+        $product->brand_id = $request->brand_id;
+        $product->category_id = $request->category_id;
+        $product->save();
+
+        // if check product has images upload
+        if($request->hasFile('product_images')){
+            $productImages = $request->file('product_images');
+            foreach($productImages as $image){
+                // Generate a unique name for the image using timestamp and random string
+                $uniqueName = time(). '-' . Str::random(10) . '.' . $image->getClientOriginalExtension();
+                $image->move('product_images', $uniqueName);
+
+                // Create a new product image record with the product_id and unique name
+                ProductImage::create([
+                    'product_id' => $product->id,
+                    'image' => 'product_images/' . $uniqueName
+                ]);
+            }
+        }    
+
+        return redirect()->route('admin.products.index')->with("success", "Product Created Successfully!");
+
+
     }
 }
