55070652be5fda5cf36b4cea8687f85cfcd67d80
unknown <csesujon155@gmail.com>
2024-09-18 18:41:02 -0700

    Authenticated Login and User Register

--- resources/js/Pages/User/Layouts/Header.vue
+++ resources/js/Pages/User/Layouts/Header.vue
@@ -1,13 +1,23 @@
+<script setup>
+
+    import { usePage, Link } from '@inertiajs/vue3';
+    const canLogin = usePage().props.canLogin;
+    const canRegister = usePage().props.canRegister;
+    const auth = usePage().props.auth;
+
+</script>
 <template>
      <!-- header start -->
      <div class="container mx-auto my-3">
         <nav class="bg-white border-gray-200 dark:bg-gray-900">
             <div class="max-w-screen-xxl flex flex-wrap items-center justify-between mx-auto p-4">
+                
                 <a href="https://codersujon.com/" class="flex items-center space-x-3 rtl:space-x-reverse"
                     target="_blank">
                     <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">VShop</span>
                 </a>
+
                 <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
                     <ul
                         class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
@@ -34,8 +44,10 @@
                         </li>
                     </ul>
                 </div>
-                <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <button type="button"
+
+                <div v-if="canLogin" class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    
+                    <button type="button" v-if="auth.user"
                         class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
                         id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
                         data-dropdown-placement="bottom">
@@ -43,6 +55,14 @@
                         <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg"
                             alt="user photo">
                     </button>
+
+                    <!-- login and register -->
+                    <div v-else>
+                        <Link :href="route('login')" type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Login</Link>
+
+                        <Link :href="route('register')" v-if="canRegister" type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Register</Link>
+                    </div>
+
                     <!-- Dropdown menu -->
                     <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
                         id="user-dropdown">
@@ -82,11 +102,10 @@
                         </svg>
                     </button>
                 </div>
+
             </div>
         </nav>
     </div>
     <!-- end -->
 </template>
-<script setup>
 
-</script>
